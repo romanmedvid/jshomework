df38669d7e2df46487d4ceb7e1ee24d0f6cadb86
Roman Medvid <roman_medvyd@ukr.net>
2018-11-21 08:44:34 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+alert(solveQr(a ,b, c));
+
+function solveQr(a, b, c,){
+  const d = discrCalc(a, b, c);
+  if (d < 0) {
+    return "no result";
+}
+  if (d == 0) {
+    let x = ((-b - d) / (2 * a));
+    return x;
+}
+
+  if (d > 0) {
+    let x1 = ((-b + d) / (2 * a));
+    let x2 = ((-b - d) / (2 * a));
+    return 'x1=' + x1 + ',x2=' + x2;
+}
+}
+function discrCalc(a, b, c) {
+    return ((b * b) - 4 * (a * c));
+    }
